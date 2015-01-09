23c9f7121465da83b51ee3be034152ed5897d3d8
thinking-aloud <lennart.k@thinking-aloud.eu>
2015-01-09 13:11:53 +0100

    You can shoot yourself.

---
+++ src/main/ChordClient.java
@@ -0,0 +1,91 @@
+package main;
+
+import de.uniba.wiai.lspi.chord.data.URL;
+import de.uniba.wiai.lspi.chord.service.NotifyCallback;
+import de.uniba.wiai.lspi.chord.service.PropertiesLoader;
+import de.uniba.wiai.lspi.chord.service.ServiceException;
+import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.Inet4Address;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ChordClient {
+
+    private static ChordImpl chordImpl;
+//    private static final StringKey myKey = new StringKey("Just an example.");
+    private static final String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
+    private static final BigInteger biggestNodeKey = new BigDecimal(2.0).pow(160).toBigInteger().subtract(BigInteger.ONE);
+    private static final String ip = "localhost";
+
+    public static void main(String[] args) {
+        joinChord();
+
+        setShips();
+
+        shoot("1");
+        shoot("2");
+
+    }
+
+    private static void joinChord() throws RuntimeException {
+        PropertiesLoader.loadPropertyFile();
+        URL localURL = null;
+        try {
+            localURL = new URL(protocol + "://" + Inet4Address.getLocalHost().getHostAddress() + ":8181/");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (UnknownHostException e) {
+            Logger.getLogger(ChordClient.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        chordImpl = new ChordImpl();
+        NotifyCallback myNotifyCallback = new GameNotify();
+        chordImpl.setCallback(myNotifyCallback);
+
+        URL bootstrapURL = null;
+        try {
+            bootstrapURL = new URL(protocol + "://" + ip + ":8080/");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            chordImpl.join(localURL, bootstrapURL);
+        } catch (ServiceException e) {
+            throw new RuntimeException("Could not join DHT!", e);
+        }
+
+        System.out.println("Chord joined: " + bootstrapURL + "\n");
+
+//        System.out.println("MaxID:         " + biggestNodeKey);
+//        System.out.println("NodeId:        " + chordImpl.getID());
+//        System.out.println("PredecessorID: " + chordImpl.getPredecessorID());
+    }
+
+    // man müsste die Schiffe natürlich auf den zugewiesenen Bereich verteilen.
+    private static void setShips() {
+        chordImpl.insert(new StringKey("1"), "Schiff");
+        chordImpl.insert(new StringKey("3"), "Schiff");
+        chordImpl.insert(new StringKey("7"), "Schiff");
+        chordImpl.insert(new StringKey("9"), "Schiff");
+    }
+
+    // Man schießt noch auf sich selber, aber geht das irgendwie in die richtige Richtung?
+    private static void shoot(String sektor) {
+        StringKey key = new StringKey(sektor);
+
+        if (chordImpl.retrieve(key).size() > 0) {
+            chordImpl.remove(key, "Schiff");
+            System.out.println("Schiff in Sektor " + sektor + " getroffen!");
+            chordImpl.broadcast(chordImpl.getID(), Boolean.TRUE); // Broadcast wird noch nicht ausgewertet
+        } else {
+            System.out.println("Kein Schiff in Sektor " + sektor + "!");
+            chordImpl.broadcast(chordImpl.getID(), Boolean.FALSE);
+        }
+    }
+
+}
--- src/main/ChordHost.java
+++ src/main/ChordHost.java
@@ -14,10 +14,11 @@ import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class CreateChord {
+public class ChordHost {
 
 	private static final String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
-	private static final BigInteger biggestNodeKey = new BigDecimal(2.0).pow(160).toBigInteger().subtract(BigInteger.ONE);
+    private static final BigInteger biggestNodeKey = new BigDecimal(2.0).pow(160).toBigInteger().subtract(BigInteger.ONE);
+    private static int numberOfShips = 0;
 
 	public static void main(String[] args) {
 		PropertiesLoader.loadPropertyFile();
@@ -27,7 +28,7 @@ public class CreateChord {
 		} catch (MalformedURLException e) {
 			throw new RuntimeException(e);
 		} catch (UnknownHostException e) {
-			Logger.getLogger(CreateChord.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(ChordHost.class.getName()).log(Level.SEVERE, null, e);
 		}
 
 		ChordImpl chordImpl = new ChordImpl();
@@ -38,24 +39,29 @@ public class CreateChord {
 			chordImpl.create(localURL);
 		} catch (ServiceException e) {
 			throw new RuntimeException("Could not create DHT!", e);
-		}
+        }
 
-		System.out.println("MaxID:  " + biggestNodeKey);
-		System.out.println("NodeId: " + chordImpl.getID().toBigInteger());
+        System.out.println("Chord listens on: " + localURL + "\n");
 
-		String data = "Just an example.";
-		StringKey myKey = new StringKey(data);
+//		System.out.println("MaxID:  " + biggestNodeKey);
+//        System.out.println("NodeId: " + chordImpl.getID().toBigInteger());
+//        System.out.println("PredecessorID: " + chordImpl.getPredecessorID());
 
-		while (true) {
-			try {
-				Thread.sleep(1000);
-				if (chordImpl.retrieve(myKey).size() > 0) {
-					System.out.println(chordImpl.retrieve(myKey));
-				}
-			} catch (Exception e) {
-				System.out.println("stacktrace:");
-				System.out.println(Arrays.toString(e.getStackTrace()));
-			}
-		}
+//		String data = "Just an example.";
+//		StringKey myKey = new StringKey(data);
+//
+//		while (true) {
+//			try {
+//                Thread.sleep(1000);
+//                // just prints result, when the number of entries has changed (stops spaming console)
+//                if (chordImpl.retrieve(myKey).size() != numberOfShips) {
+//                    System.out.println(chordImpl.retrieve(myKey));
+//                    numberOfShips = chordImpl.retrieve(myKey).size();
+//                }
+//			} catch (Exception e) {
+//				System.out.println("stacktrace:");
+//				System.out.println(Arrays.toString(e.getStackTrace()));
+//			}
+//		}
 	}
 }
