dce82ee7400c24a3a26aacd0bf471e33b7a6d20f
lennart <lennart@ws-84-123.haw-1x.haw-hamburg.de>
2015-01-12 17:32:33 +0100

    sector creation fixed

--- src/main/ChordClient.java
+++ src/main/ChordClient.java
@@ -1,16 +1,18 @@
 package main;
 
+import de.uniba.wiai.lspi.chord.data.ID;
 import de.uniba.wiai.lspi.chord.data.URL;
 import de.uniba.wiai.lspi.chord.service.NotifyCallback;
 import de.uniba.wiai.lspi.chord.service.PropertiesLoader;
 import de.uniba.wiai.lspi.chord.service.ServiceException;
 import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
-import java.math.BigDecimal;
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.net.Inet4Address;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import java.util.Random;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,32 +21,61 @@ public class ChordClient {
 	private static ChordImpl chordImpl;
 	private static final String protocol = URL.KNOWN_PROTOCOLS
 			.get(URL.SOCKET_PROTOCOL);
-	private static final BigInteger biggestNodeKey = new BigDecimal(2.0)
-			.pow(160).toBigInteger().subtract(BigInteger.ONE);
+	private static final BigInteger biggestID = BigInteger.valueOf(2).pow(160).subtract(BigInteger.ONE);
     private static final String ip = "localhost";
 
-    private static final BigInteger[] sectors = new BigInteger[100];
+	private static final BigInteger[] sectors = new BigInteger[100];
     private static final boolean[] ships = new boolean[100];
 
 	public static void main(String[] args) {
         joinChord();
         calculateSectors();
-        setShips();
+		setShips();
+
+		System.out.println("maxID :   " + biggestID);
 
 
-        shoot(0);
-        shoot(1);
-        shoot(2);
-        shoot(3);
-        shoot(4);
-        shoot(5);
-        shoot(6);
-        shoot(7);
-        shoot(8);
-        shoot(9);
-        shoot(10);
+//		System.out.println("biggestID length:  " + biggestID.bitCount());
+//
+//		byte[] test = new byte[biggestID.toByteArray().length];
+//		System.arraycopy(biggestID.toByteArray(), 0, test, 0, biggestID.toByteArray().length);
+//		System.out.println("biggestID length2: " + test.length);
+//
+//		ID test = new ID(biggestID.toByteArray());
+//
+//		System.out.println("value: " + test);
+//		System.out.println("value: " + biggestID.toByteArray().length);
+//		System.out.println("length: " + test.getLength());
+
+//		shoot(test);
+
+//		shoot(new ID(sectors[0].toByteArray()));
+//		shoot(new ID(sectors[1].toByteArray()));
+//		shoot(new ID(sectors[2].toByteArray()));
+//		shoot(new ID(sectors[3].toByteArray()));
+//		shoot(new ID(sectors[4].toByteArray()));
+//		shoot(new ID(sectors[5].toByteArray()));
+//		shoot(new ID(sectors[6].toByteArray()));
+//		shoot(new ID(sectors[7].toByteArray()));
+//		shoot(new ID(sectors[8].toByteArray()));
+//		shoot(new ID(sectors[9].toByteArray()));
+//		shoot(new ID(sectors[10].toByteArray()));
     }
 
+	public static int byteArrayToInt(byte[] b) {
+		int value = 0;
+		for (int i = 0; i < 4; i++) {
+			int shift = (4 - 1 - i) * 8;
+			value += (b[i] & 0x000000FF) << shift;
+		}
+		return value;
+	}
+
+	/**
+	 * joins the Chord Network
+	 *
+	 * @throws RuntimeException
+	 */
 	private static void joinChord() throws RuntimeException {
 		PropertiesLoader.loadPropertyFile();
 		URL localURL = null;
@@ -89,25 +120,31 @@ public class ChordClient {
 	}
 
     /**
-     * Habe mit Mitja geredet, er sagte mir, dass wir den Bereich von unsere ID zu der des
-     * vorherigen nutzen sollen. Wir müssen wohl diesen Bereich in 100 Teile teilen und 10 davon mit
-     * Schiffen besetzen.
-     */
+     * Calculates the sectores between the predecessor and our id and splits this into 100 Sectors. Also handles the case if the predecessor has a bigger id than we do.
+	 */
     private static void calculateSectors() {
-
+		BigInteger distance;
         BigInteger myID = chordImpl.getID().toBigInteger();
         BigInteger predecessorID = chordImpl.getPredecessorID().toBigInteger();
-//        System.out.println("myID:          " + myID);
-//        System.out.println("predecessorID: " + predecessorID);
+//		System.out.println("myID:          " + myID);
+//		System.out.println("predecessorID: " + predecessorID);
 
-        BigInteger distance = myID.subtract(predecessorID);
-        BigInteger step = distance.divide(BigInteger.valueOf(100L));
+		// predecessorID might be bigger than our ID, due to Chord circle
+		if (myID.compareTo(predecessorID) > 0) {
+			distance = myID.subtract(predecessorID);
+		} else {
+			distance = biggestID.subtract(predecessorID).add(myID);
+		}
+//		System.out.println("distance:      " + distance);
 
-        for (int i = 0; i < 100; i++) {
-            sectors[i] = BigInteger.valueOf((long) i).multiply(step);
-//            System.out.println("sector " + (i + 1) + ": " + sectors[i]);
-        }
+		BigInteger step = distance.divide(BigInteger.valueOf(100L));
 
+		for (int i = 0; i < 100; i++) {
+			// (predecessorID + (i * step) + 1) % biggestID
+			sectors[i] = predecessorID.add(BigInteger.valueOf((long) i).multiply(step)).add(BigInteger.ONE).mod(biggestID);
+
+//			System.out.println("sector " + (i + 1) + ": " + sectors[i]);
+		}
     }
 
     /**
@@ -119,23 +156,16 @@ public class ChordClient {
         int random;
 
         for (int i = 0; i < 10; i++) {
-
             do {
                 random = rnd.nextInt(100);
             } while (ships[random] == true);
 
             ships[random] = true;
-            chordImpl.insert(new StringKey(sectors[random].toString()), "ship");
         }
 
-        for (int i = 0; i < 100; i++) {
-            System.out.println("ship " + (i + 1) + ": " + ships[i]);
-        }
-
-//		chordImpl.insert(new StringKey("1"), "Schiff");
-//		chordImpl.insert(new StringKey("3"), "Schiff");
-//		chordImpl.insert(new StringKey("7"), "Schiff");
-//		chordImpl.insert(new StringKey("9"), "Schiff");
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println("ship " + (i + 1) + ": " + ships[i]);
+//        }
     }
 
     /**
@@ -143,18 +173,26 @@ public class ChordClient {
      *
      * @param sector
      */
-    private static void shoot(int sector) {
-        StringKey key = new StringKey(sectors[sector].toString());
+	private static void shoot(ID sector) {
+		Set<Serializable> result = chordImpl.retrieve(sector);
 
-        if (ships[sector]) {
-			chordImpl.remove(key, "Schiff");
-            System.out.println("Schiff in Sektor " + (sector + 1) + " getroffen!");
-			//wird noch nicht ausgewertet
-			chordImpl.broadcast(chordImpl.getID(), Boolean.TRUE);
-		} else {
-            System.out.println("Kein Schiff in Sektor " + (sector + 1) + "!");
-			chordImpl.broadcast(chordImpl.getID(), Boolean.FALSE);
-		}
+		result.stream().forEach((s) -> {
+			System.out.println(s);
+		});
+	}
+
+	private static void gotHit() {
+//        StringKey key = new StringKey(sectors[sector].toString());
+//
+//        if (ships[sector]) {
+//			chordImpl.remove(key, "Schiff");
+//            System.out.println("Schiff in Sektor " + (sector + 1) + " getroffen!");
+//			//wird noch nicht ausgewertet
+//			chordImpl.broadcast(chordImpl.getID(), Boolean.TRUE);
+//		} else {
+//            System.out.println("Kein Schiff in Sektor " + (sector + 1) + "!");
+//			chordImpl.broadcast(chordImpl.getID(), Boolean.FALSE);
+//		}
 	}
 
 }
