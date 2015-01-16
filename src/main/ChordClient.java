88f450e6d8c2a7db29a4d19e669a766e53436a60
repat <repat@repat.de>
2015-01-16 11:54:32 +0100

    chordclient mit join und create, refactoring

--- src/main/ChordClient.java
+++ src/main/ChordClient.java
@@ -2,6 +2,7 @@ package main;
 
 import de.uniba.wiai.lspi.chord.data.ID;
 import de.uniba.wiai.lspi.chord.data.URL;
+import de.uniba.wiai.lspi.chord.service.NotifyCallback;
 import de.uniba.wiai.lspi.chord.service.PropertiesLoader;
 import de.uniba.wiai.lspi.chord.service.ServiceException;
 import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
@@ -15,37 +16,50 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ChordClient implements Runnable {
-    // constants
+    // constants for config
     private static final String PROTOCOL = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
-    private static final String PORT = "8080";
-	private static final ID BIGGESTID = new ID(BigInteger.valueOf(2).pow(160).subtract(BigInteger.ONE).toByteArray());
-	private static final String IP = "localhost";
-	private static final int numberOfPlayers = 2;
-	private static final int S = 10; // number of ships
-	private static final int I = 100; // number of mySectors
-	private static final boolean TESTING_MODE = false;
+    private static final String HOST_PORT = "8080";
+    private static final String STARTING_PORT = "8181";
+    private static final String HOST_IP = "192.168.1.1";
+    private static final String LOCAL_IP = "192.168.1.2";
+    private static final int NUMBER_OF_PLAYERS = 2;
+    private static final int NUMBER_OF_THREADS = 1;
+    private static final int S = 10; // number of ships
+    private static final int I = 100; // number of mySectors
+    private static final boolean TESTING_MODE = true;
+    private static final int WAITING_TIME_CHORD_JOIN = 10000;
+    private static final int MIDDLE = 2;
 
-	private final String PORT_LOCAL;
-	private ChordImpl chordImpl;
-	ID[] mySectors = new ID[I];
-	final boolean[] ships = new boolean[I];
-	private ID myID;
-	private ID distance;
-	private final String PLAYER_NAME; // just for local testing purposos
-	private final Scanner scanner = new Scanner(System.in);
-	private GameNotify myNotifyCallback;
+    // instances of things
+    private ChordImpl chordImpl;
+    private ID distance;
+    private final Scanner scanner = new Scanner(System.in);
+    private GameNotify myNotifyCallback;
+    private String localPort = "0";
+    private String joinOrCreate = "join";
+    private static String JOIN_OR_CREATE_FOR_THIS_CLIENT = "create";
 
-    public ChordClient(String PORT_LOCAL, String playerName) {
-        this.PORT_LOCAL = PORT_LOCAL;
+    // package private
+    ID myID;
+    ID[] mySectors = new ID[I];
+    final boolean[] ships = new boolean[I];
+    static final ID BIGGEST_ID = new ID(BigInteger.valueOf(2).pow(160).subtract(BigInteger.ONE).toByteArray());
+    final String PLAYER_NAME; // just for local testing purposos
+
+    public ChordClient(String localPort, String playerName, String joinOrCreate) {
+        this.localPort = localPort;
         this.PLAYER_NAME = playerName;
+        this.joinOrCreate = joinOrCreate;
     }
 
     public static void main(String[] args) throws InterruptedException {
 
         // spawns x numbers of clients
-        for (int i = 0; i < 1; i++) {
-            String port = String.valueOf(8181 + i);
-            new Thread(new ChordClient(port, "Player " + (i + 1))).start();
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            // TODO: unsinn
+            String port = String.valueOf(Integer.valueOf(STARTING_PORT) + i);
+            
+            new Thread(new ChordClient(port, "Player " + (i + 1), JOIN_OR_CREATE_FOR_THIS_CLIENT)).start();
             // sleep prevents calling "loadPropertyFile()" at the same time.
             Thread.sleep(100);
         }
@@ -53,21 +67,27 @@ public class ChordClient implements Runnable {
 
     @Override
     public void run() {
-        joinChord();
 
-        if (!TESTING_MODE) {
+        if (joinOrCreate == "join") {
+            joinChord();
+        } else if (joinOrCreate == "create") {
+            createChord();
+        }
+
+        if (TESTING_MODE) {
             // start the game after s was typed in
             String input;
             do {
                 System.out.print("type \"s\" to begin: ");
                 input = scanner.next();
             } while (!input.equals("s"));
+            scanner.close();
         }
 
-        // waits 10000 sec to make sure others have joined chord
+        // waits 10000 msec to make sure others have joined chord
         try {
-            if (!TESTING_MODE) {
-                Thread.sleep(10000);
+            if (TESTING_MODE) {
+                Thread.sleep(WAITING_TIME_CHORD_JOIN);
             } else {
                 Thread.sleep(3000);
             }
@@ -75,21 +95,21 @@ public class ChordClient implements Runnable {
             Logger.getLogger(ChordClient.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-		myID = chordImpl.getID();
-		mySectors = calculateSectors(chordImpl.getPredecessorID(), myID);
-		setShips();
+        myID = chordImpl.getID();
+        mySectors = calculateSectors(chordImpl.getPredecessorID(), myID);
+        setShips();
 
-		// we start the game if we have the BIGGESTID
-		if (chordImpl.getPredecessorID().compareTo(chordImpl.getID()) > 0) {
-			System.out.println(PLAYER_NAME + ": I start!");
+        // we start the game if we have the BIGGESTID
+        if (chordImpl.getPredecessorID().compareTo(chordImpl.getID()) > 0) {
+            System.out.println(PLAYER_NAME + ": I start!");
 
-			shoot();
-		}
-	}
+            shoot();
+        }
+    }
 
     /**
      * joins the Chord Network
-     *
+     * 
      * @throws RuntimeException
      */
     private void joinChord() throws RuntimeException {
@@ -98,12 +118,17 @@ public class ChordClient implements Runnable {
         }
         URL localURL = null;
         try {
-            localURL = new URL(PROTOCOL + "://" + Inet4Address.getLocalHost().getHostAddress() + ":" + PORT_LOCAL + "/");
+            // localURL = new URL(PROTOCOL + "://" +
+            // Inet4Address.getLocalHost().getHostAddress() + ":" + localPort +
+            // "/");
+            localURL = new URL(PROTOCOL + "://" + LOCAL_IP + ":" + localPort + "/");
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
-        } catch (UnknownHostException e) {
-            Logger.getLogger(ChordClient.class.getName()).log(Level.SEVERE, null, e);
         }
+        // } catch (UnknownHostException e) {
+        // Logger.getLogger(ChordClient.class.getName()).log(Level.SEVERE, null,
+        // e);
+        // }
 
         chordImpl = new ChordImpl();
         myNotifyCallback = new GameNotify();
@@ -112,7 +137,7 @@ public class ChordClient implements Runnable {
 
         URL bootstrapURL = null;
         try {
-            bootstrapURL = new URL(PROTOCOL + "://" + IP + ":" + PORT + "/");
+            bootstrapURL = new URL(PROTOCOL + "://" + HOST_IP + ":" + HOST_PORT + "/");
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -127,31 +152,32 @@ public class ChordClient implements Runnable {
         // System.out.println("Chord joined: " + bootstrapURL + "\n");
     }
 
-	/**
-	 * Calculates the mySectors between the predecessor and our id and splits this into I Sectors. Also handles the case if the predecessor has a bigger id than we do.
-	 *
-	 * @param from the first id, thinking circle clockwise
-	 * @param to the second id,
-	 * @return array of BigInteger
-	 */
-	private ID[] calculateSectors(ID from, ID to) {
-		ID[] result = new ID[I];
+    /**
+     * Calculates the mySectors between the predecessor and our id and splits
+     * this into I Sectors. Also handles the case if the predecessor has a
+     * bigger id than we do.
+     * 
+     * @param from
+     *            the first id, thinking circle clockwise
+     * @param to
+     *            the second id,
+     * @return array of BigInteger
+     */
+    private ID[] calculateSectors(ID from, ID to) {
+        ID[] result = new ID[I];
 
-		// predecessorID might be bigger than our ID, due to Chord circle
+        // predecessorID might be bigger than our ID, due to Chord circle
         if (from.compareTo(to) < 0) {
             distance = to.subtract(from);
         } else {
-            distance = BIGGESTID.subtract(from).add(to);
+            distance = BIGGEST_ID.subtract(from).add(to);
         }
 
-		ID step = distance.divide(I);
+        ID step = distance.divide(I);
 
         for (int i = 0; i < I; i++) {
-			// (from + 1 + (i * step)) % biggestID
-			result[i] = from
-				.add(1)
-				.add(step.multiply(i))
-				.mod(BIGGESTID);
+            // (from + 1 + (i * step)) % biggestID
+            result[i] = from.add(1).add(step.multiply(i)).mod(BIGGEST_ID);
 
             // System.out.println("sector " + (i + 1) + ": " + result[i]);
         }
@@ -173,57 +199,64 @@ public class ChordClient implements Runnable {
 
             ships[random] = true;
         }
-	}
-
-	void shoot() {
-
-		System.out.println();
-		Random rnd = new Random();
-		long sectorNumber;
-		ID sectorSize = getBIGGESTID().divide(3 * 100);
-		ID target;
-
-		do {
-			sectorNumber = rnd.nextInt(100 * numberOfPlayers); // number of sectors * players
-			target = sectorSize
-				.multiply(sectorNumber)
-				.add((sectorSize.divide(2)))
-				.mod(getBIGGESTID());
-		} while (isMyID(target) || alreadyHit(target));
-
-		System.out.println(PLAYER_NAME + ": shooting at: " + target.toBigInteger());
-		chordImpl.retrieve(target);
-	}
-
-	boolean isMyID(ID id) {
-		boolean result = id.isInInterval(mySectors[0], myID);
-//		if (result) {
-//			System.out.println(PLAYER_NAME + ": " + id + " is my own ID");
-//		}
-		return result;
-	}
-
-	private boolean alreadyHit(ID id) {
-
-		for (BroadcastLog b : myNotifyCallback.getBroadcastLog()) {
-			if (b.getTarget().equals(id)) {
-//				System.out.println(getPLAYER_NAME() + ": already hit this target!");
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public ID getMyID() {
-        return myID;
     }
 
-    public String getPLAYER_NAME() {
-        return PLAYER_NAME;
+    void shoot() {
+
+        System.out.println();
+        Random rnd = new Random();
+        long sectorNumber;
+        ID sectorSize = BIGGEST_ID.divide(I * NUMBER_OF_PLAYERS);
+        ID target;
+
+        do {
+            // number of sectors * players
+            sectorNumber = rnd.nextInt(I * NUMBER_OF_PLAYERS);
+            target = sectorSize.multiply(sectorNumber).add((sectorSize.divide(MIDDLE))).mod(BIGGEST_ID);
+        } while (isInMyInterval(target) || isAlreadyHit(target));
+
+        System.out.println(PLAYER_NAME + ": shooting at: " + target.toBigInteger());
+        chordImpl.retrieve(target);
     }
 
-	public ID getBIGGESTID() {
-		return BIGGESTID;
-	}
+    boolean isInMyInterval(ID id) {
+        boolean result = id.isInInterval(mySectors[0], myID);
+        // if (result) {
+        // System.out.println(PLAYER_NAME + ": " + id + " is my own ID");
+        // }
+        return result;
+    }
 
+    private boolean isAlreadyHit(ID id) {
+
+        for (BroadcastLog b : myNotifyCallback.getBroadcastLog()) {
+            if (b.getTarget().equals(id)) {
+                // System.out.println(getPLAYER_NAME() +
+                // ": already hit this target!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void createChord() {
+        URL localURL = null;
+        try {
+            localURL = new URL(PROTOCOL + "://" + HOST_IP + ":" + HOST_PORT + "/");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        
+        ChordImpl chordImpl = new ChordImpl();
+        NotifyCallback myNotifyCallback = new GameNotify();
+        chordImpl.setCallback(myNotifyCallback);
+
+        try {
+            chordImpl.create(localURL);
+        } catch (ServiceException e) {
+            throw new RuntimeException("Could not create DHT!", e);
+        }
+
+        System.out.println("Chord listens on: " + localURL + "\n");
+    }
 }
